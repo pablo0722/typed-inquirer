24d73899c2b1cfaca6a1c180f0c9fb8b6e5ff05a
Pablo Rodrigo D'angelo <pablo.dangelo@famar.com>
2023-01-09 00:00:35 -0300

    retrieve typed inquirer

--- types/fbjs/lib/Locale.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Locale {
-    function isRTL(): boolean;
-}
-
-// eslint-disable-next-line export-just-namespace
-export = Locale;
--- types/openui5/sap.ui.dt.d.ts
+++
@@ -1,3 +0,0 @@
-// For Library Version: 1.109.0
-
-declare namespace sap {}
